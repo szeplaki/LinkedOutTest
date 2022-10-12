3ad57b44920b80e7b94836c0c0e9caf895e05be2
Z-SooS <zoli.soos98@gmail.com>
2022-10-12 15:06:44 +0200

    Created student registration model

---
+++ src/main/java/org/example/Model/register/StudentRegisterModel.java
@@ -0,0 +1,94 @@
+package org.example.Model.register;
+
+import org.example.FileReader;
+import org.example.Model.BaseModel;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class StudentRegisterModel extends BaseModel {
+    @FindBy(xpath = "//*[@id='root']//div[@class='welcome']")
+    private WebElement welcomeScreen;
+
+
+    @FindBy(id = "name")
+    private WebElement nameField;
+
+    @FindBy(id = "age")
+    private WebElement ageField;
+
+    @FindBy(id = "email")
+    private WebElement emailField;
+
+    @FindBy(id = "phone")
+    private WebElement phoneField;
+
+    @FindBy(id = "psw")
+    private WebElement passwordField;
+
+    @FindBy(id = "psw_again")
+    private WebElement passwordAgainField;
+
+    @FindBy(id = "city")
+    private WebElement cityField;
+
+    @FindBy(id = "register-btn")
+    private WebElement registerButton;
+
+    public void clickRegisterButton() {
+        registerButton.click();
+    }
+
+    public void setName(String name) {
+        nameField.clear();
+        nameField.sendKeys(name);
+    }
+
+    public void setAge(String age) {
+        ageField.clear();
+        ageField.sendKeys(age);
+    }
+
+    public void setEmailField(String email) {
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
+
+    public void setPhone(String phone) {
+        phoneField.clear();
+        phoneField.sendKeys(phone);
+    }
+    public void setPassword(String password) {
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+
+    public void setPasswordAgain(String passwordAgain) {
+        passwordAgainField.clear();
+        passwordAgainField.sendKeys(passwordAgain);
+    }
+
+    public void setCity(String city) {
+        cityField.clear();
+        cityField.sendKeys(city);
+    }
+
+    public void openForm() {
+        webDriver.get(FileReader.getValueByKeyFromConfigProperties("linkedout.baseurl") + "/student/registration");
+    }
+
+    public boolean isMainPage() {
+        WebDriverWait shortWait = new WebDriverWait(webDriver, Duration.ofSeconds(3));
+        try {
+            shortWait.until(ExpectedConditions.visibilityOf(welcomeScreen));
+            return true;
+        }
+        catch (TimeoutException ignore) {
+            return false;
+        }
+    }
+}
